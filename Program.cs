da9f23c10723db80c59967f4b492f47b88a3a0e5
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-09-17 10:52:50 -0300

    nameninja

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Write("Escreva seu nome: ");
+string nomedigitado = Console.ReadLine()!;
+string nomeninja = nomedigitado
+            .ToUpper()
+            .Replace("A" , "ka")
+            .Replace("B" , "zu")
+            .Replace("C" , "mi")
+            .Replace("D" , "te")
+            .Replace("E" , "ku")
+            .Replace("F" , "lu")
+            .Replace("G" , "ji")
+            .Replace("H" , "ri")
+            .Replace("I" , "ki")
+            .Replace("J" , "zus")
+            .Replace("K" , "me")
+            .Replace("L" , "ta")
+            .Replace("M" , "rin")
+            .Replace("N" , "to")
+            .Replace("O" , "mo")
+            .Replace("P" , "no")
+            .Replace("Q" , "ke")
+            .Replace("R" , "shi")
+            .Replace("S" , "ari")
+            .Replace("T" , "shi")
+            .Replace("U" , "do")
+            .Replace("V" , "ru")
+            .Replace("W" , "mei")
+            .Replace("X" , "na")
+            .Replace("Y" , "fu")
+            .Replace("Z" , "zi");
+
+      string nomeprimeiro = nomeninja.Substring(0,1).ToUpper();
+      string nomeminusculo = nomeninja.Substring(1).ToLower();
+      Console.Write($"Seu nome ninja é {nomeprimeiro}{nomeminusculo}");
+
+
+
+
+
+
+
+
+
+
